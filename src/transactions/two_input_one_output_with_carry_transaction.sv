73e00bcc3b8384c03bbb0fe1fc6e425d1b25f0ac
Andrew Parlane <andrew.parlane@gmail.com>
2016-07-11 13:03:31 -0400

    Transactions: Added a new transaction that extends two_input_with_carry_transaction by adding outputs.

---
+++ src/transactions/two_input_one_output_with_carry_transaction.sv
@@ -0,0 +1,32 @@
+`include "uvm_macros.svh"
+
+package two_input_one_output_with_carry_transaction_pkg;
+    import uvm_pkg::*;
+    import two_input_with_carry_transaction_pkg::*;
+
+    // we extend the two_input_with_carry_transaction
+    // and just add the output and output carry
+    class two_input_one_output_with_carry_transaction #(parameter INPUT_WIDTH, parameter OUTPUT_WIDTH) extends two_input_with_carry_transaction #(.WIDTH(INPUT_WIDTH));
+        `uvm_object_param_utils(two_input_one_output_with_carry_transaction #(INPUT_WIDTH, OUTPUT_WIDTH))
+
+        logic [OUTPUT_WIDTH-1:0] out;
+        logic carryOut;
+
+        function new(string name = "two_input_one_output_with_carry_transaction");
+            super.new(name);
+        endfunction: new
+
+        function string output2string();
+            string s;
+            s = $sformatf(" out=%h, carryOut=%h", out, carryOut);
+            return s;
+        endfunction: output2string
+
+        function string convert2string();
+            string s;
+            s = { super.convert2string(), output2string() };
+            return s;
+        endfunction: convert2string
+
+    endclass: two_input_one_output_with_carry_transaction
+endpackage: two_input_one_output_with_carry_transaction_pkg
