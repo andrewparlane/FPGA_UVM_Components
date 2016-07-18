a78f989744eab6342aebbd4fe4668bb672cf7227
Andrew Parlane <andrew.parlane@gmail.com>
2016-07-17 23:06:48 -0400

    TwoInputWithCarry Transaction: Using the UVM field macros so we can use the copy method.

--- src/transactions/two_input_with_carry_transaction.sv
+++ src/transactions/two_input_with_carry_transaction.sv
@@ -4,11 +4,15 @@ package two_input_with_carry_transaction_pkg;
     import uvm_pkg::*;
 
     class two_input_with_carry_transaction #(parameter WIDTH = 1) extends uvm_sequence_item;
-        `uvm_object_param_utils(two_input_with_carry_transaction #(WIDTH));
-
         rand bit [WIDTH-1:0] in1;
         rand bit [WIDTH-1:0] in2;
         rand bit carryIn;
+
+        `uvm_object_param_utils_begin(two_input_with_carry_transaction #(WIDTH))
+            `uvm_field_int(in1, UVM_ALL_ON)
+            `uvm_field_int(in2, UVM_ALL_ON)
+            `uvm_field_int(carryIn, UVM_ALL_ON)
+        `uvm_object_utils_end
 
         function new(string name = "two_input_with_carry_transaction");
             super.new(name);
