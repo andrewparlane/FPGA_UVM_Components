b04b73c6d904600f42c12007da4a63125ab03c3f
Andrew Parlane <andrew.parlane@gmail.com>
2016-07-26 19:50:25 -0400

    Transactions: Adding the implementations for do_copy, do_compare and convert2string methods.

--- src/transactions/two_input_one_output_with_carry_transaction_pkg.sv
+++ src/transactions/two_input_one_output_with_carry_transaction_pkg.sv
@@ -16,6 +16,7 @@ package two_input_one_output_with_carry_transaction_pkg;
             super.new(name);
         endfunction: new
 
+        // custom methods ----------------------------------------------------------
         function string outputs2string();
             string s;
             s = $sformatf(" out=%h, carryOut=%h", out, carryOut);
@@ -23,11 +24,52 @@ package two_input_one_output_with_carry_transaction_pkg;
         endfunction: outputs2string
 
         function void copy_inputs(two_input_with_carry_transaction #(.WIDTH(INPUT_WIDTH)) tx);
-            super.copy(tx);
+            super.do_copy(tx);
         endfunction: copy_inputs
 
         function logic compare_outputs(two_input_one_output_with_carry_transaction #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) tx);
             return (out === tx.out && carryOut === tx.carryOut);
         endfunction: compare_outputs
+
+        // fill in the default transaction functions -------------------------------
+        function void do_copy(uvm_object rhs);
+            two_input_one_output_with_carry_transaction #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) castedRhs;
+
+            // cast the generic uvm_object to our type
+            // if it fails, then the object passed in was not of a valid type
+            if (!$cast(castedRhs, rhs)) begin
+                `uvm_error("two_input_one_output_with_carry_transaction", "do_copy $cast() failed");
+                return;
+            end
+
+            // get our parent to do their work first (typical of children)
+            super.do_copy(rhs);
+
+            // copy our items over
+            out = castedRhs.out;
+            carryOut = castedRhs.carryOut;
+        endfunction: do_copy
+
+        function bit do_compare(uvm_object rhs, uvm_comparer comparer);
+            two_input_one_output_with_carry_transaction #(.INPUT_WIDTH(INPUT_WIDTH), .OUTPUT_WIDTH(OUTPUT_WIDTH)) castedRhs;
+
+            // cast the generic uvm_object to our type
+            // if it fails, then the object passed in was not of a valid type
+            if (!$cast(castedRhs, rhs)) begin
+                `uvm_error("two_input_one_output_with_carry_transaction", "do_compare $cast() failed");
+                return 0;
+            end
+
+            // check if our parent compares OK first
+            // then compare our fields
+            return (super.do_compare(rhs, comparer) &&
+                    out === castedRhs.out &&
+                    carryOut === castedRhs.carryOut);
+        endfunction: do_compare
+
+        function string convert2string();
+            return { super.convert2string(), " ", outputs2string() };
+        endfunction: convert2string
+
     endclass: two_input_one_output_with_carry_transaction
 endpackage: two_input_one_output_with_carry_transaction_pkg
--- src/transactions/two_input_with_carry_transaction_pkg.sv
+++ src/transactions/two_input_with_carry_transaction_pkg.sv
@@ -4,25 +4,64 @@ package two_input_with_carry_transaction_pkg;
     import uvm_pkg::*;
 
     class two_input_with_carry_transaction #(parameter WIDTH = 1) extends uvm_sequence_item;
+        `uvm_object_param_utils(two_input_with_carry_transaction #(WIDTH))
+
         rand bit [WIDTH-1:0] in1;
         rand bit [WIDTH-1:0] in2;
         rand bit carryIn;
-
-        `uvm_object_param_utils_begin(two_input_with_carry_transaction #(WIDTH))
-            `uvm_field_int(in1, UVM_ALL_ON)
-            `uvm_field_int(in2, UVM_ALL_ON)
-            `uvm_field_int(carryIn, UVM_ALL_ON)
-        `uvm_object_utils_end
 
         function new(string name = "two_input_with_carry_transaction");
             super.new(name);
         endfunction: new
 
+        // custom methods ----------------------------------------------------------
         function string inputs2string();
             string s;
             s = $sformatf("in1=%h, in2=%h, carryIn=%h", in1, in2, carryIn);
             return s;
         endfunction: inputs2string
+
+        // fill in the default transaction functions -------------------------------
+        function void do_copy(uvm_object rhs);
+            two_input_with_carry_transaction #(.WIDTH(WIDTH)) castedRhs;
+
+            // cast the generic uvm_object to our type
+            // if it fails, then the object passed in was not of a valid type
+            if (!$cast(castedRhs, rhs)) begin
+                `uvm_error("two_input_with_carry_transaction", "do_copy $cast() failed");
+                return;
+            end
+
+            // get our parent to do their work first (typical of children)
+            super.do_copy(rhs);
+
+            // copy our items over
+            in1 = castedRhs.in1;
+            in2 = castedRhs.in2;
+            carryIn = castedRhs.carryIn;
+        endfunction: do_copy
+
+        function bit do_compare(uvm_object rhs, uvm_comparer comparer);
+            two_input_with_carry_transaction #(.WIDTH(WIDTH)) castedRhs;
+
+            // cast the generic uvm_object to our type
+            // if it fails, then the object passed in was not of a valid type
+            if (!$cast(castedRhs, rhs)) begin
+                `uvm_error("two_input_with_carry_transaction", "do_compare $cast() failed");
+                return 0;
+            end
+
+            // check if our parent compares OK first
+            // then compare our fields
+            return (super.do_compare(rhs, comparer) &&
+                    in1 === castedRhs.in1 &&
+                    in2 === castedRhs.in2 &&
+                    carryIn === castedRhs.carryIn);
+        endfunction: do_compare
+
+        function string convert2string();
+            return { super.convert2string(), " ", inputs2string() };
+        endfunction: convert2string
 
     endclass: two_input_with_carry_transaction
 endpackage: two_input_with_carry_transaction_pkg
